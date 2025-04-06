d7346b62dbd837efd81d226e6d54c80c33c71580
Arielly Parussulo <gitlab-ci-token@opera.com>
2025-04-06 16:07:49 +0200

    Add heroku config

--- server/src/server.ts
+++ server/src/server.ts
@@ -19,6 +19,7 @@ app.get("/", (_req: Request, res: Response) => {
 
 app.use("/api/highscores", routes);
 
-app.listen(3000, () => {
-  console.log(`Server running on port 3000`);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
